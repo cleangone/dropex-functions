c17b15c5acc44cbef595e61477d1efc4ec06e9ad
Andy Robbins <andyrobbins@Splashtop.local>
2020-08-24 14:51:27 -0400

    Mark invoice as Sent once email sent

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -116,27 +116,17 @@ async function updateDropItem(change: any, context: any) {
          log.info("Updating " + dropItemDesc)
          const dropItemUpdate = { status: DROPITEM_HOLD, buyerId: dropItem.currBidderId, buyerName: userFullName }               
          return dropItemRef.update(dropItemUpdate).then(() => {        
-            const authUserDesc = "authUser[id: " + userId + "]"
-            log.info("Getting " + authUserDesc)
-            return admin.auth().getUser(userId).then(userRecord => {
-               log.info("Creating email")
+            const timerDesc = "timers[id: " + context.params.timerId + "]"
+            console.log("Deleting " + timerDesc) 
+            return change.after.ref.delete().then(() => {   
                const htmlMsg =  
                   "You are the high bidder on item <a href=http://dropex.4th.host>" + dropItem.name + "</a>" + 
                   "<p>You will be contacted with the location of the alley in which to deliver the briefcase full of cash</p>"
-               const email =  { 
-                  to: [userRecord.email],
-                  from: "Dropmaster <dropmaster@4th.host>",
-                  message: { subject: "Winning bid", html: htmlMsg }
-               }
-                  
-               return db.collection("emails").add(email).then(() => {   
-                  const timerDesc = "timers[id: " + context.params.timerId + "]"
-                  console.log("Deleting " + timerDesc) 
-                  return change.after.ref.delete() 
-               })
-               .catch(error => { return logError("Error adding Email", error) })
+               const subject = "Winning bid"
+               
+               return sendEmail(userId, subject, htmlMsg)
+               .catch(error => { return logError("Error sending Email", error) }) 
             })
-            .catch(error => { return logError("Error getting " + authUserDesc, error) })
          })
          .catch(error => { return logError("Error updating " + dropItemDesc, error) })
       })
@@ -144,6 +134,62 @@ async function updateDropItem(change: any, context: any) {
    })
    .catch(error => { return logError("Error getting " + dropItemDesc, error) })
 }
+
+export const processInvoice = functions.firestore
+   .document('invoices/{invoiceId}')
+   .onCreate((snapshot, context) => {
+   const invoice = snapshot.data()
+   if (!invoice) { return logError("Invoice does not exist") }
+
+   const invoiceDesc = "invoices[id: " + invoice.id + "]"
+   let itemText = ''
+   for (var item of invoice.items) {
+      if (itemText.length > 0) { itemText += ", " }
+      itemText += item.name
+   }
+
+   const htmlMsg = "Here is you invoice for <a href=http://dropex.4th.host>" + itemText + "</a>" 
+   return sendEmail(invoice.userId, "Invoice", htmlMsg).then(() => {
+      console.log("Updating invoice " + invoiceDesc)
+      return snapshot.ref.update({ status: "Sent", sentDate: Date.now() })
+   })
+   .catch(error => { return logError("Error sending Email", error) }) 
+})
+
+
+async function sendEmail(userId: string, subject: string, htmlMsg: string) {
+   const authUserDesc = "authUser[id: " + userId + "]"
+   log.info("Getting " + authUserDesc)
+
+   return admin.auth().getUser(userId).then(userRecord => {
+      log.info("Creating email")
+      const email =  { 
+         to: [userRecord.email],
+         from: "Dropmaster <dropmaster@4th.host>",
+         message: { subject: subject, html: htmlMsg }
+      }
+      return db.collection("emails").add(email)
+      .catch(error => { throw logReturnError("Error adding Email", error) })   
+   })
+   .catch(error => { throw logReturnError("Error getting " + authUserDesc, error) })
+}
+
+// async function sendEmailOld(userId: string, subject: string, htmlMsg: string) {
+//    const authUserDesc = "authUser[id: " + userId + "]"
+//    log.info("Getting " + authUserDesc)
+   
+//    return admin.auth().getUser(userId).then(userRecord => {
+//       log.info("Creating email")
+//       const email =  { 
+//          to: [userRecord.email],
+//          from: "Dropmaster <dropmaster@4th.host>",
+//          message: { subject: subject, html: htmlMsg }
+//       }
+//       return db.collection("emails").add(email)
+//       .catch(error => { return logError("Error adding Email", error) })   
+//    })
+//    .catch(error => { return logError("Error getting " + authUserDesc, error) })
+// }
 
 // convenience methods to log and return
 function logInfo(msg: string) {
@@ -156,4 +202,11 @@ function logError(msg: string, error: any = null) {
    else { log.error(msg) }
 
    return null
+}
+
+function logReturnError(msg: string, error: any) {
+   if (error) { log.error(msg, error)}
+   else { log.error(msg) }
+
+   return error
 }
